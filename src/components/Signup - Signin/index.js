da62753a5c674fd1c373d0811e8876e622c8341e
sandeep18p <sandeep.pansari98@gmail.com>
2024-07-10 16:20:37 +0530

    three_3

--- src/components/Signup - Signin/index.js
+++ src/components/Signup - Signin/index.js
@@ -52,7 +52,41 @@ const SigninSignup = () => {
         });
     }
 
-    function signInWithGoogle() {}
+    function signInWithGoogle() {
+      setLoading(true);
+      try {
+        signInWithPopup(auth, provider)
+          .then((result) => {
+            // This gives you a Google Access Token. You can use it to access the Google API.
+            const credential = GoogleAuthProvider.credentialFromResult(result);
+            const token = credential.accessToken;
+            // The signed-in user info.
+            const user = result.user;
+            console.log("op ",user)
+            createDoc(user);
+            setLoading(false);
+            navigate("/dashboard")
+            toast.success("Google Authenticated!");
+            // IdP data available using getAdditionalUserInfo(result)
+            // ...
+          })
+          .catch((error) => {
+            // Handle Errors here.
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            // The email of the user's account used.
+            const email = error.customData?.email;
+            // The AuthCredential type that was used.
+            const credential = GoogleAuthProvider.credentialFromError(error);
+            toast.error(error.message);
+            setLoading(false);
+            // ...
+          });
+      } catch (error) {
+        setLoading(false);
+        toast.error(error.message);
+      }
+    }
     async function signupWithEmail() {
       setLoading(true);
   
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,16 +1,44 @@
-import react from "react";
+import react, { useEffect } from "react";
 import styles from "./styles.css"
+import { toast } from "react-toastify";
+import { signOut } from "firebase/auth";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useNavigate } from "react-router-dom";
+import { auth } from "../../firebase";
+// import userImg from '../../assets/user.jpg'
 
-function logout() {
-    // auth.signOut();
-    // navigate("/");
-  }
+
+
 
 const Header =()=>{
+
+  const [user, loading] = useAuthState(auth);
+  const navigate = useNavigate();
+  function logout() {
+    signOut(auth)
+      .then(() => {
+        toast.success("Logged out!");
+        navigate("/");
+      })
+      .catch((error) => {
+        toast.error(error.message);
+      });
+  }
+
+  useEffect(() => {
+    if (user) {
+      navigate("/dashboard");
+    }
+  }, [user, loading]);
     return(<>
      <div className="navbar">
      <p className="logo">Financely</p>
-     <p className="logo link" onClick={logout}>logout</p>
+     {user && (<div className="logo_div">
+        {/* <img src={user.photoURL ? user.photoURL:userImg} alt="" /> */}
+        <p onClick={logout} className="link">
+          Logout
+        </p>
+        </div>)}
      </div>
     </>)
 }
